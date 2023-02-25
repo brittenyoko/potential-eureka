8e1ee634eb2216f8494d7f2937ccdd029332f083
Britteny Okorom-Achuonye <bokoromachuonye@college.harvard.edu>
2023-02-25 01:12:40 -0500

    add todo logic

--- eureka/src/App.js
+++ eureka/src/App.js
@@ -1,15 +1,108 @@
-import './App.css';
-import React from 'react'
-import ListInput from './components/ListInput';
+import "./App.css";
+
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  display: inline-block;
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+  height: 30px;
+  width: 50px;
+  border-radius: 2px;
+  cursor: pointer;
+`;
+const Text = styled.input`
+  border: 2px solid #000;
+`;
+const TaskCount = styled.span`
+  margin: 10px;
+`;
+const Tasks = styled.div``;
+const LIST = styled.li`
+  liststyle: "none";
+  text-decoration: "line-through";
+`;
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
+  const handleClick = () => {
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        task: input,
+        complete: false,
+      },
+    ]);
+    setInput("");
+  };
+
+  const handleComplete = (id) => {
+    let list = todoList.map((task) => {
+      let item = {};
+      if (task.id === id) {
+        if (!task.complete) {
+          //Task is pending, modifying it to complete and increment the count
+          setCompletedTaskCount(completedTaskCount + 1);
+        } else {
+          //Task is complete, modifying it back to pending, decrement Complete count
+          setCompletedTaskCount(completedTaskCount - 1);
+        }
+        item = { ...task, complete: !task.complete };
+      } else item = { ...task };
+      return item;
+    });
+    setTodoList(list);
+  };
+
   return (
-    <div className="main-container">
-      <h1 className="main-heading">
-        To Do App
-      </h1>
-      <ListInput />
-    </div>
+    <Container>
+      <div>
+        <h2>Todo List</h2>
+        <Text value={input} onInput={(e) => setInput(e.target.value)} />
+        <Button onClick={() => handleClick()}>Add</Button>
+        <Tasks>
+          <TaskCount>
+            <b>Pending Tasks</b>
+          </TaskCount>
+          <TaskCount>
+            <b>Completed Tasks</b>
+          </TaskCount>
+        </Tasks>
+        <div>
+          <ul>
+            {todoList.map((todo) => {
+              return (
+                <LIST
+                  complete={todo.complete}
+                  id={todo.id}
+                  onClick={() => handleComplete(todo.id)}
+                  style={{
+                    listStyle: "none",
+                    textDecoration: todo.complete && "line-through",
+                  }}
+                >
+                  {todo.task}
+                </LIST>
+              );
+            })}
+          </ul>
+          ;
+        </div>
+        <Button>Clear</Button>
+      </div>
+    </Container>
   );
 }
 
--- eureka/src/components/ListInput.jsx
+++ eureka/src/components/ListInput.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import * as React from "react";
+import Post from './Post.jsx'
 
 function ListInput() {
   const [inputValue, setInputValue] = useState("");
+  const groceryList = []; 
 
   const onSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${inputValue}`);
+    alert(`The item you entered was: ${inputValue}`);
+    groceryList.push(inputValue);
+    
   }
   
 
   return (
-    <form onSubmit={onSubmit} >
+    <>
+     <form onSubmit={onSubmit} >
       <label>Enter your name:
         <input
           type="text" 
@@ -20,6 +25,14 @@ function ListInput() {
         <input type='submit' />
       </label>
     </form>
+
+    <div className="posts-container">
+	{groceryList.map((post, index) => (
+		<Post key={index} index={index} post={post} />
+	))}
+	</div>
+    </>
+   
   );
 }
 
